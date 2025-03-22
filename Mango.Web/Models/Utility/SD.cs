ed9fe80aac82fd8b24c15b94e321a38bc767eec6
avinashdash94 <20avinashdash@gmail.com>
2025-03-23 00:15:01 +0530

    Added Few models used to call Coupon API fromfront end app. Careated base http service class to ssend any type of request.

---
+++ Mango.Web/Models/Utility/SD.cs
@@ -0,0 +1,14 @@
+﻿namespace Mango.Web.Models.Utility
+{
+    //This class constains static details used in the project
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
